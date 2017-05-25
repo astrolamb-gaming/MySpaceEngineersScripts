150988263fd92292be44aa4bb60cf2a2493c5e29
astrolamb-gaming <astrolamb.gaming@gmail.com>
2017-05-25 00:53:49 -0400

    Update LandCraft.cs

--- LandCraft.cs
+++ LandCraft.cs
@@ -1,5 +1,7 @@
 // Copy this function directly into your script, then call the function in your Main() method.
 
+// WIP
+
 public void Land() {
   var lg = (IMyLandingGear) GridTerminalSystem.GetBlockWithName("Landing Gear");
   var rc = (IMyRemoteControl) GridTerminalSystem.GetBlockWithName("Remote Control);
@@ -7,12 +9,15 @@ public void Land() {
   
   if (sensor == null) {
     var sensor = (IMySensor) GridTerminalSystem.GetBlockWithName("LandingSensor");
+    if (sensor == null) {
+      return;
+    }
     
     
     
     
  } else {
-  sensor.SetCustomName("LandingSensor");
+    sensor.SetCustomName("LandingSensor");
  }
   
   
